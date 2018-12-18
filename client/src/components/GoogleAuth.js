a26e92dbf722a33595cb7ba6121c6c44f67bd24e
Matt Cataldi <mcataldi@apics.org>
2018-12-18 08:37:45 -0600

    Google Auth work.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,8 +4,7 @@ class GoogleAuth extends React.Component {
   componentdidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client.init({
-        clientId:
-          "557178508348-gs7apm28a7omu57vgdjrr25k1h14qsfu.apps.googleusercontent.com",
+        clientId: "hidden",
         scope: "email"
       });
     });
